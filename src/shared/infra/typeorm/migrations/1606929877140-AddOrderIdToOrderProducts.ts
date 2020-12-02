97bb7d726f0766f156f8227bb0dcd4700b8b70a4
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-12-02 14:25:14 -0300

    Created customer foreign key on order

---
+++ src/shared/infra/typeorm/migrations/1606929877140-AddOrderIdToOrderProducts.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddOrderIdToOrderProducts1606929877140 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
